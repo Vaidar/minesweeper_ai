69e8bfd6521b1cea3badfbfda9f12319ad9d25b3
Vaidar <vidar.borjesson.19@gmail.com>
2021-11-07 14:11:09 +0100

    Fixade en broken grej. Funkar lite bättre nu.

--- main.c
+++ main.c
@@ -5,42 +5,30 @@
 #include "game.h"
 #include "minesweeper_ai.h"
 
-#define SLEEP_TIME 1000000
+#define SLEEP_TIME 100000
 
 void exit_game(game *g, int status);
 
+// Otroligt optimiserad. Kollar alla rutor hela tiden flera gånger i onödan.
 int main(void) {
     srand(time(NULL));
-    game *g = game_new(20, 20, 75);
+    game *g = game_new(25, 25, 100);
     minesweeper_ai *ai = calloc(1, sizeof(minesweeper_ai));
+    
     if (guess_reveal(g) != 0) {
         exit_game(g, 1);
     }
     while (1) {
-        system("clear");
-        draw_board(g);
-        usleep(SLEEP_TIME);
-
-        // Random reveals for debug purposes
-        // int x, y;
-        // do {
-        //     x = rand() % g->width;
-        //     y = rand() % g->height; 
-        // } while (g->board[y][x].state != CELL_STATE_HIDDEN);
-        // reveal_cell(g, x, y);
-
-        // system("clear");
-        // draw_board(g);
-        for (int i = 0; i < g->width; i++) {
-            for (int j = 0; j < g->height; j++) {
-                check_if_flag(j, i, g->board, g->width, g->height);
-                if (try_reveal_cell(ai, j, i, g) != 0) {
+        for (int y = 0; y < g->height; y++) {
+            for (int x = 0; x < g->width; x++) {
+                check_if_flag(x, y, g->board, g->width, g->height);
+                if (try_reveal_cell(ai, x, y, g) != 0) {
                     exit_game(g, 1);
                 }
             }
-            //system("clear");
-            //draw_board(g);
-            //usleep(1000000);
+            system("clear");
+            draw_board(g);
+            usleep(SLEEP_TIME);
         }
         if (ai->has_to_guess == 0) {
             if (guess_reveal(g) != 0) {
@@ -56,9 +44,8 @@ int main(void) {
 void exit_game(game *g, int status) {
     system("clear");
     draw_board(g);
-    if (status != 0) {
-        fprintf(stderr, "You lost!\n");
-    }
+    if (status != 0) fprintf(stderr, "You lost!\n");
+
     game_free(g);
     printf("\n");
     exit(status);
